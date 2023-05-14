2ee6d3de4896f2741d877a79bda57e122739081b
iNQU1SITOR <sankevich2003@mail.ru>
2023-05-14 23:47:55 +0300

    No

--- src/main/java/com/example/hotels/repositories/DirectorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hotels.repositories;
-
-import com.example.hotels.models.Director;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DirectorRepository extends JpaRepository<Director, Long> {
-}
